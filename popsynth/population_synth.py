b18b61ae57956906963dd64433dfdbc617bb39f1
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-10-11 13:37:14 +0200

    comments

--- popsynth/population_synth.py
+++ popsynth/population_synth.py
@@ -24,28 +24,40 @@ class PopulationSynth(object):
     __metaclass__ = abc.ABCMeta
     
     def __init__(self, r_max=10, seed = 1234, name='no_name'):
+        """
+        
+        """
         self._n_model = 500
         self._seed = int(seed)
         self._model_spaces = {}
 
         self._name = name
 
- 
-        #self._lf_params = {}
-        #self._spatial_params = {}
-
         self._r_max = r_max
         
     def set_luminosity_function_parameters(self, **lf_params):
+        """
+        Set the luminosity function parameters as keywords
+        """
 
         self._lf_params = lf_params
 
     def set_spatial_distribution_params(self, **spatial_params):
-
+        """
+        Set the spatial parameters as keywords
+        """
         self._spatial_params = spatial_params
 
     def add_model_space(self, name, start, stop, log=True):
+        """
+        Add a model space for stan generated quantities
+    
+        :param name: name that Stan will use
+        :param start: start of the grid
+        :param stop: stop of the grid
+        :param log: use log10 or not
 
+        """
         if log:
             space = np.logspace(np.log10(start), np.log10(stop), self._n_model)
 
@@ -58,7 +70,9 @@ class PopulationSynth(object):
     @property
     def name(self):
         return self._name
-        
+
+    # The following methods must be implemented in subclasses
+    
     @abc.abstractmethod
     def phi(self, L):
         pass
@@ -77,27 +91,39 @@ class PopulationSynth(object):
         return 1.
 
     def draw_distance(self, size):
+        """
+        Draw the distances from the specified dN/dr model
+        """
 
+        # create a callback for the sampler
         dNdr = lambda r: self.dNdV(r) * self.differential_volume(r) / self.time_adjustment(r)
 
+        # find the maximum point
         tmp = np.linspace(0, self._r_max, 1E5)
-
         ymax = np.max(dNdr(tmp))
 
+        # rejection sampling the distribution
         r_out = []
-
         with progress_bar(size, title='Drawing distances') as pbar:
             for i in range(size):
                 flag = True
                 while flag:
 
+                    # get am rvs from 0 to the max of the function
+                    
                     y = np.random.uniform(low=0, high=ymax)
+
+                    # get an rvs from 0 to the maximum distance
+                    
                     r = np.random.uniform(low=0, high=self._r_max)
 
+                    # compare them
+                    
                     if y < dNdr(r):
                         r_out.append(r)
                         flag = False
                 pbar.increase()
+                
         return np.array(r_out)
 
     @abc.abstractmethod
@@ -109,13 +135,25 @@ class PopulationSynth(object):
         pass
 
     def prob_det(self, x, boundary, strength):
+        """
+        Soft detection threshold
+
+        :param x: values to test
+        :param boundary: mean value of the boundary
+        :param strength: the strength of the threshold
+        """
+
 
         return sf.expit(strength * (x - boundary))
 
     def draw_log10_fobs(self, f, f_sigma, size=1):
+        """
+        draw the log10 of the the fluxes
+        """
 
         log10_f = np.log10(f)
 
+        # sample from the log distribution to keep positive fluxes
         log10_fobs = log10_f + np.random.normal(loc=0, scale=f_sigma, size=size)
 
         return log10_fobs
