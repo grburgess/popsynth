b0d6a62a7b8b99cd30341bc42a662bae003fc8f2
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-11-23 11:12:41 +0100

    added data file copy

--- popsynth/utils/package_data.py
+++ popsynth/utils/package_data.py
@@ -1,6 +1,6 @@
 import pkg_resources
 import os
-
+from shutil import copyfile
 
 def get_path_of_data_file(data_file):
     file_path = pkg_resources.resource_filename("popsynth", 'data/%s' % data_file)
@@ -12,6 +12,15 @@ def get_path_of_data_dir():
     file_path = pkg_resources.resource_filename("popsynth", 'data')
 
     return file_path
+
+def copy_package_data(data_file):
+
+    data_file = get_path_of_data_file(data_file)
+    copyfile(data_file, '.')
+    
+
+    
+
 
 
 # def get_path_of_user_dir():
