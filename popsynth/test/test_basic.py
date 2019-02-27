f7772b4458cb6522b5793d9d51bee2d1e01fafa5
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-02-27 14:01:03 +0100

    add aux tests

--- popsynth/test/test_basic.py
+++ popsynth/test/test_basic.py
@@ -28,3 +28,46 @@ def test_basic_population():
 
 
     sfr_synth = popsynth.synths.ParetoSFRPopulation(r0=10., rise=.1, decay=2., peak=5., Lmin=1E52, alpha=1.,seed=123)
+
+
+
+def test_auxiliary_sampler():
+
+    sfr_synth = popsynth.synths.ParetoSFRPopulation(r0=10., rise=.1, decay=2., peak=5., Lmin=1E52, alpha=1.,seed=123)
+
+    class DemoSampler(popsynth.AuxiliarySampler):
+
+        def __init__(self, mu=2, tau=1., sigma=1):
+
+            self._mu = mu
+            self._tau=tau
+
+            super(DemoSampler, self).__init__('demo', sigma, observed=False)
+
+        def true_sampler(self, size):
+
+            self._true_values =  np.random.normal(self._mu, self._tau, size=size)
+
+
+
+    class DemoSampler2(popsynth.AuxiliarySampler):
+
+        def __init__(self, mu=2, tau=1., sigma=1):
+
+            self._mu = mu
+            self._tau=tau
+
+            super(DemoSampler2, self).__init__('demo2', sigma, observed=True)
+
+        def true_sampler(self, size):
+
+
+            secondary = self._secondary_samplers['demo']
+
+
+
+            self._true_values =  (np.random.normal(self._mu , self._tau, size=size)) + secondary.true_values - np.log10(1+self._distance)
+
+        def observation_sampler(self, size):
+
+            self._obs_values =  self._true_values + np.random.normal(0, self._sigma, size=size)
