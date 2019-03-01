0dd943581ac5f0a59b7eae959eba4d6d36124756
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-03-01 15:10:59 +0100

    typo

--- popsynth/synths.py
+++ popsynth/synths.py
@@ -256,5 +256,5 @@ class ParetoMaduaPopulation(ParetoPopulation, MadauPopulation):
 
         ParetoPopulation.__init__(
             self, Lmin=Lmin, alpha=alpha, r_max=r_max, seed=seed, name='ParetoMaduaPopulation')
-        ConstantSphericalPopulation.__init__(
+        MadauPopulation.__init__(
             self, r0=r0, r_max=r_max, seed=seed, name='ParetoMaduaPopulation')
