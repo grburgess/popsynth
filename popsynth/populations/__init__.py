eeb795cfcac9564fc0ea5b501be726b60ff7e2d7
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-04-07 19:34:47 +0200

    updated imports to new system

--- popsynth/populations/__init__.py
+++ popsynth/populations/__init__.py
@@ -1,3 +1,43 @@
-from .spatial_populations import SphericalPopulation, SFRPopulation, ZPowerSphericalPopulation
+from .spatial_populations import (
+    SphericalPopulation,
+    SFRPopulation,
+    ZPowerSphericalPopulation,
+)
+from .pareto_populations import (
+    ParetoHomogeneousSphericalPopulation,
+    ParetoSFRPopulation,
+    ParetoZPowerSphericalPopulation,
+)
 
-__all__ = ['SphericalPopulation', 'SFRPopulation', 'ZPowerSphericalPopulation']
+from .schechter_populations import (
+    SchechterHomogeneousSphericalPopulation,
+    SchechterSFRPopulation,
+    SchechterZPowerSphericalPopulation,
+)
+
+from .lognormal_population import (
+    Log10NormalSFRPopulation,
+    Log10NormalSFRPopulation,
+    Log10NormalZPowerSphericalPopulation,
+    LogNormalSFRPopulation,
+    LogNormalSFRPopulation,
+    LogNormalZPowerSphericalPopulation,
+)
+
+__all__ = [
+    "SphericalPopulation",
+    "SFRPopulation",
+    "ZPowerSphericalPopulation",
+    "ParetoHomogeneousSphericalPopulation",
+    "ParetoSFRPopulation",
+    "ParetoZPowerSphericalPopulation",
+    "SchechterHomogeneousSphericalPopulation",
+    "SchechterSFRPopulation",
+    "SchechterZPowerSphericalPopulation",
+    "Log10NormalHomogeneousSphericalPopulation",
+    "Log10NormalSFRPopulation",
+    "Log10NormalZPowerSphericalPopulation",
+    "LogNormalHomogeneousSphericalPopulation",
+    "LogNormalSFRPopulation",
+    "LogNormalZPowerSphericalPopulation",
+]
