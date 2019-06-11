a1904fb3a5b514c8b879b21fa4bf25ba0c959bd8
grburgess <jmichaelburgess@gmail.com>
2019-06-11 14:00:22 +0200

    added hdf5 utils and moved rejection sample to its own file

---
+++ popsynth/utils/rejection_sample.py
@@ -0,0 +1,37 @@
+from numba import jit, njit, prange, float64
+import numpy as np
+
+@jit(parallel=True, fastmath=True)
+def rejection_sample(size, ymax, xmax, func):
+    """FIXME! briefly describe function
+
+    :param size: 
+    :param ymax: 
+    :param xmax: 
+    :param func: 
+    :returns: 
+    :rtype: 
+
+    """
+
+    r_out = []
+
+    for i in prange(size):
+        flag = True
+        while flag:
+
+            # get am rvs from 0 to the max of the function
+
+            y = np.random.uniform(low=0, high=ymax)
+
+            # get an rvs from 0 to the maximum distance
+
+            r = np.random.uniform(low=0, high=xmax)
+
+            # compare them
+
+            if y < func(r):
+                r_out.append(r)
+                flag = False
+
+    return r_out
