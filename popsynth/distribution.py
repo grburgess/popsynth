1b5748c2f22de5a3df245e52f54c536b1b5f14b6
grburgess <jmichaelburgess@gmail.com>
2019-06-11 15:09:05 +0200

    add the right progress bar

--- popsynth/distribution.py
+++ popsynth/distribution.py
@@ -3,8 +3,8 @@ import abc
 
 
 from popsynth.utils.rejection_sample import rejection_sample
-from popsynth.utils.progress_bar import progress_bar
 
+from tqdm.autonotebook import tqdm as progress_bar
 class Distribution(object):
     def __init__(self, name, seed, form, truth={}):
         """FIXME! briefly describe function
