514f3f7065c052e8498e8ddd7eff73f0175c50ae
grburgess <jmichaelburgess@gmail.com>
2019-08-19 14:18:06 +0200

    test 2

--- docs/conf.py
+++ docs/conf.py
@@ -91,6 +91,9 @@ latex_elements = {
     # 'figure_align': 'htbp',
 }
 
+
+master_doc = 'index'
+
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
