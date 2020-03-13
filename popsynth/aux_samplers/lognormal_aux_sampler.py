3efb5cb0b37c1e3286e92890a6903be863d6a930
grburgess <jmichaelburgess@gmail.com>
2020-03-13 19:22:56 +0100

    added helper aux samplers

---
+++ popsynth/aux_samplers/lognormal_aux_sampler.py
@@ -0,0 +1,41 @@
+import scipy.stats as stats
+
+from popsynth.auxiliary_sampler import AuxiliarySampler
+
+
+class LogNormalAuxSampler(popsynth.AuxiliarySampler):
+    def __init__(self, name, mu=0.0, tau=1.0, sigma=None, observed=True):
+        """FIXME! briefly describe function
+
+        :param name: 
+        :param mu: 
+        :param tau: 
+        :param sigma: 
+        :param observed: 
+        :returns: 
+        :rtype: 
+
+        """
+
+        self._mu = mu
+        self._tau = tau
+
+        super(LogNormalAuxSampler, self).__init__(
+            name=name, sigma=sigma, observed=observed
+        )
+
+    def true_sampler(self, size):
+
+        self._true_values = stats.norm.rvs(loc=np.log10(self._mu), scale=self._tau, size=size)
+
+    def observation_sampler(self, size):
+
+        if self._is_observed:
+
+            self._obs_values = stats.normal.rvs(
+                loc=self._true_values, scale=self._sigma
+            )
+
+        else:
+
+            self._obs_values = self._true_values
---
+++ popsynth/aux_samplers/trunc_normal_aux_sampler.py
@@ -0,0 +1,48 @@
+import scipy.stats as stats
+
+from popsynth.auxiliary_sampler import AuxiliarySampler
+
+
+class TruncatedNormalAuxSampler(popsynth.AuxiliarySampler):
+    def __init__(self, name, lower, upper, mu=0.0, tau=1.0, sigma=None, observed=True):
+        """FIXME! briefly describe function
+
+        :param name: 
+        :param mu: 
+        :param tau: 
+        :param sigma: 
+        :param observed: 
+        :returns: 
+        :rtype: 
+
+        """
+
+        self._mu = mu
+        self._tau = tau
+        self._lower = lower
+        self._upper = upper
+
+        super(TruncatedNormalAuxSampler, self).__init__(
+            name=name, sigma=sigma, observed=observed
+        )
+
+    def true_sampler(self, size):
+
+        self._true_values = stats.truncnorm(
+            (self._lower - self._mu) / self._tau,
+            (self._upper - self._mu) / self._tau,
+            loc=self._mu,
+            scale=self._tau,
+        )
+
+    def observation_sampler(self, size):
+
+        if self._is_observed:
+
+            self._obs_values = stats.normal.rvs(
+                loc=self._true_values, scale=self._sigma
+            )
+
+        else:
+
+            self._obs_values = self._true_values
