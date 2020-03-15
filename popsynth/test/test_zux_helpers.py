f3696c03399ac682a3f0c5aae87d6f327bdb309c
grburgess <jmichaelburgess@gmail.com>
2020-03-15 17:03:52 +0100

    fix size arg

--- popsynth/test/test_zux_helpers.py
+++ popsynth/test/test_zux_helpers.py
@@ -8,67 +8,70 @@ import hypothesis.strategies as st
 
 
 @given(
-    st.floats(min_value=0.01,), st.floats(min_value=0.01,),  st.integers(min_value=1, max_value=1000)
+    st.floats(min_value=0.01,),
+    st.floats(min_value=0.01,),
+    st.integers(min_value=2, max_value=1000),
 )
-def test_lognorm_sampler(mu, tau ,size):
+def test_lognorm_sampler(mu, tau, size):
 
-    sampler = LogNormalAuxSampler('test', mu, tau, observed=False)
-
-    sampler.true_sampler(size)
-
-    sampler.observation_sampler(size)
-    
-    assert len(sampler._true_values) == size
-
-    
-    sampler = LogNormalAuxSampler('test', mu, tau, sigma=1.,  observed=True)
+    sampler = LogNormalAuxSampler("test", mu, tau, observed=False)
 
     sampler.true_sampler(size)
 
     sampler.observation_sampler(size)
-    
+
     assert len(sampler._true_values) == size
+
+    sampler = LogNormalAuxSampler("test", mu, tau, sigma=1.0, observed=True)
+
+    sampler.true_sampler(size)
+
+    sampler.observation_sampler(size)
+
+    assert len(sampler._true_values) == size
+
 
 @given(
-    st.floats(), st.floats(min_value=0.01,),  st.integers(min_value=1, max_value=1000)
+    st.floats(), st.floats(min_value=0.01,), st.integers(min_value=2, max_value=1000)
 )
-def test_norm_sampler(mu, tau ,size):
+def test_norm_sampler(mu, tau, size):
 
-    sampler = NormalAuxSampler('test', mu, tau, observed=False)
-
-    sampler.true_sampler(size)
-
-    sampler.observation_sampler(size)
-    
-    assert len(sampler._true_values) == size
-
-    
-    sampler = NormalAuxSampler('test', mu, tau, sigma=1.,  observed=True)
+    sampler = NormalAuxSampler("test", mu, tau, observed=False)
 
     sampler.true_sampler(size)
 
     sampler.observation_sampler(size)
-    
+
     assert len(sampler._true_values) == size
+
+    sampler = NormalAuxSampler("test", mu, tau, sigma=1.0, observed=True)
+
+    sampler.true_sampler(size)
+
+    sampler.observation_sampler(size)
+
+    assert len(sampler._true_values) == size
+
 
 @given(
-    st.floats(), st.floats(min_value=0.01,),  st.integers(min_value=1, max_value=1000)
+    st.floats(), st.floats(min_value=0.01,), st.integers(min_value=2, max_value=1000)
 )
-def test_truncnorm_sampler(mu, tau ,size):
+def test_truncnorm_sampler(mu, tau, size):
 
-    sampler = TruncatedNormalAuxSampler('test', -10, 10,  mu, tau, observed=False)
+    sampler = TruncatedNormalAuxSampler("test", -10, 10, mu, tau, observed=False)
 
     sampler.true_sampler(size)
 
     sampler.observation_sampler(size)
-    
+
     assert len(sampler._true_values) == size
 
-    
-    sampler = TruncatedNormalAuxSampler('test', 0, 10, mu, tau, sigma=1.,  observed=True)
+    sampler = TruncatedNormalAuxSampler(
+        "test", 0, 10, mu, tau, sigma=1.0, observed=True
+    )
 
     sampler.true_sampler(size)
 
     sampler.observation_sampler(size)
-    
+
     assert len(sampler._true_values) == size
