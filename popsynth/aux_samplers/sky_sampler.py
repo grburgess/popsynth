83edbaa207eab4fb618f5c5dae7fd5eeb4e6d76e
grburgess <jmichaelburgess@gmail.com>
2020-04-13 16:37:21 +0200

    basic sky sampler working

--- popsynth/aux_samplers/sky_sampler.py
+++ popsynth/aux_samplers/sky_sampler.py
@@ -1,18 +1,31 @@
+import numpy as np
+
 from popsynth.auxiliary_sampler import AuxiliarySampler
 from popsynth.utils.spherical_geometry import sample_theta_phi
 
+
 class SkySampler(object):
-    def __init__(self):
+    def __init__(self, ra_sampler=None, dec_sampler=None):
+        """
+        A sky sampler that samples theta phi positions
 
-        self._ra_sampler = None
-        self._dec_sampler = None
+        :returns: 
+        :rtype: 
 
-    def _sample_sky(self):
+        """
 
-        dec, ra = sample_theta_phi()
+        self._ra_sampler = ra_sampler
+        self._dec_sampler = dec_sampler
 
-        self._dec_sampler = DecSampler(dec)
-        self._ra_sampler = RASampler(ra)
+        self._setup_sky()
+
+    def _setup_sky(self):
+
+        if self._ra_sampler is None:
+            self._ra_sampler = RASampler()
+
+        if self._dec_sampler is None:
+            self._dec_sampler = DecSampler()
 
     @property
     def ra_sampler(self):
@@ -20,33 +33,35 @@ class SkySampler(object):
         return self._ra_sampler
 
     @property
-    def dec_sampler(self)
-        
+    def dec_sampler(self):
+
+        return self._dec_sampler
+
 
 class RASampler(AuxiliarySampler):
-    def __init__(self, true_values):
+    def __init__(self):
         """
-        adds the RA values of the sky positions
+
+        Samples the RA of sky uniformly
 
         :param true_values: 
         :returns: 
         :rtype: 
 
         """
-
-        self._true_values = true_values
 
         super(RASampler, self).__init__("ra", sigma=1.0, observed=False, truth={})
 
     def true_sampler(self, size):
 
-        return self._true_values
+        self._true_values = np.random.uniform(0, 2 * np.pi, size=size)
 
 
 class DecSampler(AuxiliarySampler):
-    def __init__(self, true_values):
+    def __init__(self):
         """
-        adds the Dec value of the sky_positions
+        sampler the dec of the sky uniformly
+
 
         :param true_values: 
         :returns: 
@@ -54,10 +69,8 @@ class DecSampler(AuxiliarySampler):
 
         """
 
-        self._true_values = true_values
-
         super(DecSampler, self).__init__("dec", sigma=1.0, observed=False, truth={})
 
     def true_sampler(self, size):
 
-        return self._true_values
+        self._true_values = np.arccos(1 - 2 * np.random.uniform(0.0, 1.0, size=size))
