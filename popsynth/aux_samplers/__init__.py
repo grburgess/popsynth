6081b8b10144e2aaf762db509b114fac49f6c8b9
grburgess <jmichaelburgess@gmail.com>
2020-05-23 13:20:02 +0200

    import things

--- popsynth/aux_samplers/__init__.py
+++ popsynth/aux_samplers/__init__.py
@@ -1,3 +1,6 @@
 from .viewing_angle_sampler import ViewingAngleSampler
+from .lognormal_aux_sampler import LogNormalAuxSampler
+from .normal_aux_sampler import NormalAuxSampler
+from .trunc_normal_aux_sampler import TruncatedNormalAuxSampler
 
 __all__ = ["ViewingAngleSampler"]
