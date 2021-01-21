c79ff508369acae3345fe3be9d6fd97dcf40fd32
grburgess <jmichaelburgess@gmail.com>
2021-01-21 16:09:19 +0100

    added my own progress bar

---
+++ popsynth/utils/progress_bar.py
@@ -0,0 +1,8 @@
+from tqdm.auto import tqdm
+
+from popsynth.utils.configuration import popsynth_config
+
+
+def progress_bar(itr, **kwargs):
+
+    return (tqdm(itr, **kwargs) if popsynth_config["show_progress"] else itr)
