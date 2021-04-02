832c43b133412fb6e4df47d7163d3dd1d3aecde0
grburgess <jmichaelburgess@gmail.com>
2021-04-02 16:03:39 +0200

    add registry for distributions

---
+++ popsynth/utils/registry.py
@@ -0,0 +1,11 @@
+from class_registry import AutoRegister, ClassRegistry
+
+auxiliary_parameter_registry = ClassRegistry("_auxiliary_param_name")
+
+distribution_registry = ClassRegistry("_distribution_name")
+
+selection_registry = ClassRegistry("_selection_name")
+
+
+__all__ = ["auxiliary_parameter_registry",
+           "distribution_registry", "distribution_registry"]
