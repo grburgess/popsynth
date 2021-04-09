8d4e4894890483cd0a3a992151fc20d11afa6c36
grburgess <jmichaelburgess@gmail.com>
2021-04-09 11:43:00 +0200

    asdfasdfadsfsafasdf

--- docs/conf.py
+++ docs/conf.py
@@ -94,6 +94,10 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
+
+nbsphinx_allow_errors =True
+nbsphinx_execute = "never"
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
