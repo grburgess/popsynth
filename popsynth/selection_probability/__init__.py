fd7afed515118031f2e8aec5be32ed748fa6ca49
grburgess <jmichaelburgess@gmail.com>
2021-04-11 11:10:07 +0200

    import selection prob parameter

--- popsynth/selection_probability/__init__.py
+++ popsynth/selection_probability/__init__.py
@@ -1,8 +1,9 @@
 from .flux_selectors import HardFluxSelection, SoftFluxSelection
 from .generic_selectors import (BernoulliSelection, BoxSelection, LowerBound,
                                 SoftSelection, UnitySelection, UpperBound)
-from .selection_probability import DummySelection, SelectionProbabilty
+from .selection_probability import (DummySelection, SelectionParameter,
+                                    SelectionProbabilty)
 from .spatial_selection import SpatialSelection
 
 __all__ = ["HardFluxSelection", "SoftFluxSelection", "BernoulliSelection", "LowerBound", "UpperBound",
-                                "SoftSelection", "UnitySelection", "SelectionProbabilty", "SpatialSelection", "BoxSelection", "DummySelection"]
+                                "SoftSelection", "UnitySelection", "SelectionProbabilty", "SpatialSelection", "BoxSelection", "DummySelection", "SelectionProbabilityParameter"]
