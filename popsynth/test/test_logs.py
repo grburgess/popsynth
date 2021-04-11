24405869e051a910d2208adbbe20c70dcd7e2b16
grburgess <jmichaelburgess@gmail.com>
2021-04-11 22:43:50 +0200

    more tests

---
+++ popsynth/test/test_logs.py
@@ -0,0 +1,24 @@
+import popsynth
+
+
+def test_log_settings():
+
+    popsynth.silence_warnings()
+    popsynth.activate_warnings()
+
+
+    popsynth.silence_logs()
+
+
+    popsynth.activate_logs()
+
+
+    popsynth.show_progress_bars()
+
+    popsynth.silence_progress_bars()
+
+
+    popsynth.loud_mode()
+
+
+    popsynth.quiet_mode()
---
+++ popsynth/test/test_registry.py
@@ -0,0 +1,8 @@
+from popsynth import list_available_auxiliary_samplers, list_available_distributions, list_available_selection_functions
+
+
+def test_listings():
+
+    list_available_auxiliary_samplers()
+    list_available_distributions()
+    list_available_selection_functions()
