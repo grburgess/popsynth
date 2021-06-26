dcdc33c5765eb0a89ad8727eeb0ad43e2530eda4
Francesca Capel <capel.francesca@gmail.com>
2021-06-26 13:07:14 +0200

    Docstrings for spatial populations

--- popsynth/populations/spatial_populations.py
+++ popsynth/populations/spatial_populations.py
@@ -1,30 +1,41 @@
 from popsynth.distributions.cosmological_distribution import (  # MergerDistribution,
-    SFRDistribution, ZPowerCosmoDistribution)
+    SFRDistribution,
+    ZPowerCosmoDistribution,
+)
 from popsynth.distributions.spherical_distribution import (
-    ConstantSphericalDistribution, ZPowerSphericalDistribution)
-from popsynth.distributions.spiral_galaxy_distribution import \
-    SpiralGalaxyDistribution
+    ConstantSphericalDistribution,
+    ZPowerSphericalDistribution,
+)
+from popsynth.distribution import LuminosityDistribution
+from popsynth.distributions.spiral_galaxy_distribution import SpiralGalaxyDistribution
 from popsynth.population_synth import PopulationSynth
 
-# First create a bunch of spatial distributions
+"""
+Create a range of spatial populations that can
+be expanded with luminosity distributions.
+"""
 
 
 class SphericalPopulation(PopulationSynth):
-    def __init__(self,
-                 Lambda,
-                 r_max=5.0,
-                 seed=1234,
-                 luminosity_distribution=None):
+    def __init__(
+        self,
+        Lambda: float,
+        r_max: float = 5.0,
+        seed: int = 1234,
+        luminosity_distribution: LuminosityDistribution = None,
+    ):
         """
-        A generic spherical population
+        A generic spherical population based on
+        :class:`ConstantSphericalDistribution`.
 
-        :param Lambda:
-        :param r_max:
-        :param seed:
-        :param luminosity_distribution:
-        :returns:
-        :rtype:
-
+        :param Lambda: Density per unit volume
+        :type Lambda: float
+        :param r_max: Maximum distance
+        :type r_max: float
+        :param seed: Random seed
+        :type seed: int
+        :param luminosity_distribution: Luminosity distribution
+        :type luminosity_distribution: :class:`LuminosityDistribution`, optional
         """
 
         spatial_distribution = ConstantSphericalDistribution(seed=seed)
@@ -39,25 +50,29 @@ class SphericalPopulation(PopulationSynth):
 
 
 class ZPowerSphericalPopulation(PopulationSynth):
-    def __init__(self,
-                 Lambda,
-                 delta,
-                 r_max=5.0,
-                 seed=1234,
-                 luminosity_distribution=None):
+    def __init__(
+        self,
+        Lambda: float,
+        delta: float,
+        r_max: float = 5.0,
+        seed: int = 1234,
+        luminosity_distribution: LuminosityDistribution = None,
+    ):
         """
-        
-        A spherical population with a density that 
-        scales as (r+1)^delta
+        A spherical population with a density that
+        scales as (r+1)^delta.
+        Based on :class:`ZpowerSphericalDistribution`.
 
-        :param Lambda:
-        :param delta:
-        :param r_max:
-        :param seed:
-        :param luminosity_distribution:
-        :returns:
-        :rtype:
-
+        :param Lambda: Local density per unit volume
+        :type Lambda: float
+        :param delta: Index of spatial distribution
+        :type delta: float
+        :param r_max: Maximum distance
+        :type r_max: float
+        :param seed: Random seed
+        :type seed: int
+        :param luminosity_distribution: Luminosity distribution
+        :type luminosity_distribution: :class:`LuminosityDistribution`, optional
         """
 
         spatial_distribution = ZPowerSphericalDistribution(seed=seed)
@@ -76,27 +91,39 @@ class ZPowerSphericalPopulation(PopulationSynth):
 class ZPowerCosmoPopulation(PopulationSynth):
     def __init__(
         self,
-        Lambda,
-        delta,
-        r_max=5.0,
-        seed=1234,
-        luminosity_distribution=None,
-        is_rate=True,
+        Lambda: float,
+        delta: float,
+        r_max: float = 5.0,
+        seed: int = 1234,
+        luminosity_distribution: LuminosityDistribution = None,
+        is_rate: bool = True,
     ):
-        """FIXME! briefly describe function
+        """
+        A cosmological population with a density that
+        scales as (z+1)^delta.
+        Based on :class:`ZpowerCosmoDistribution`.
 
-        :param Lambda:
-        :param delta:
-        :param r_max:
-        :param seed:
-        :param luminosity_distribution:
-        :returns:
-        :rtype:
-
+        :param Lambda: Local density in units of Gpc^-3
+        :type Lambda: float
+        :param delta: Index of spatial distribution
+        :type delta: float
+        :param r_max: Maximum redshift
+        :type r_max: float
+        :param seed: Random seed
+        :type seed: int
+        :param luminosity_distribution: Luminosity distribution
+        :type luminosity_distribution: :class:`LuminosityDistribution`, optional
+        :param is_rate: `True` if modelling a population of transient events,
+        `False` if modelling a population of steady-state objects.
+        Affects the ``time_adjustment`` method used in cosmo calculations.
+        Default is `True`.
+        :type is_rate: bool
         """
 
-        spatial_distribution = ZPowerCosmoDistribution(seed=seed,
-                                                       is_rate=is_rate)
+        spatial_distribution = ZPowerCosmoDistribution(
+            seed=seed,
+            is_rate=is_rate,
+        )
 
         spatial_distribution.Lambda = Lambda
         spatial_distribution.r_max = r_max
@@ -119,22 +146,35 @@ class SFRPopulation(PopulationSynth):
         peak: float,
         r_max: float = 5,
         seed: int = 1234,
-        luminosity_distribution=None,
-        is_rate=True,
+        luminosity_distribution: LuminosityDistribution = None,
+        is_rate: bool = True,
     ):
-        """FIXME! briefly describe function
+        """
+        A cosmological population with a density that
+        scales similarly to the star formation rate.
+        Based on :class:`ZpowerCosmoDistribution`.
 
-        :param r0:
-        :param a:
-        :param rise:
-        :param decay:
-        :param peak:
-        :param r_max:
-        :param seed:
-        :param luminosity_distribution:
-        :returns:
-        :rtype:
-
+        :param r0: Local density in units of Gpc^-3
+        :type r0: float
+        :param a: Offset at z=0
+        :type a: float
+        :param rise: Rise at low z
+        :type rise: float
+        :param decay: Decay at high z
+        :type decay: float
+        :param peak: Peak of z distribution
+        :type peak: float
+        :param r_max: Maximum redshift
+        :type r_max: float
+        :param seed: Random seed
+        :type seed: int
+        :param luminosity_distribution: Luminosity distribution
+        :type luminosity_distribution: :class:`LuminosityDistribution`, optional
+        :param is_rate: `True` if modelling a population of transient events,
+        `False` if modelling a population of steady-state objects.
+        Affects the ``time_adjustment`` method used in cosmo calculations.
+        Default is `True`.
+        :type is_rate: bool
         """
 
         spatial_distribution = SFRDistribution(seed=seed, is_rate=is_rate)
@@ -156,15 +196,41 @@ class SFRPopulation(PopulationSynth):
 class MWRadialPopulation(PopulationSynth):
     def __init__(
         self,
-        rho,
-        a=1.64,
-        b=4.01,
-        R1=0.55,
-        R0=8.5,
-        r_max=20,
-        seed=1234,
-        luminosity_distribution=None,
+        rho: float,
+        a: float = 1.64,
+        b: float = 4.01,
+        R1: float = 0.55,
+        R0: float = 8.5,
+        r_max: float = 20,
+        seed: int = 1234,
+        luminosity_distribution: LuminosityDistribution = None,
     ):
+        """
+        A Milky-way like population based on
+        :class:`SpiralGalaxyDistribution`.
+
+        :param rho: Local density
+        :type rho: float
+        :param a: Shape parameter
+        :type a: float
+        :param b: Shape parameter
+        :type b: float
+        :param R1: Scale parameter
+        :type R1: float
+        :param R0: Scale parameter
+        :type R0: float
+        :param r_max: Maximum distance
+        :type r_max: float
+        :param seed: Random seed
+        :type seed: int
+        :param luminosity_distribution: Luminosity distribution
+        :type luminosity_distribution: :class:`LuminosityDistribution`, optional
+        :param is_rate: `True` if modelling a population of transient events,
+        `False` if modelling a population of steady-state objects.
+        Affects the ``time_adjustment`` method used in cosmo calculations.
+        Default is `True`.
+        :type is_rate: bool
+        """
 
         spatial_distribution = SpiralGalaxyDistribution(seed=seed)
 
