71767034d504aef39ab090f9c12de7c73231a2ff
Francesca Capel <capel.francesca@gmail.com>
2021-12-28 17:32:15 +0100

    New tests for secondary samplers that use distance and luminosity

--- popsynth/test/test_va_sampler.py
+++ popsynth/test/test_va_sampler.py
@@ -1,12 +1,12 @@
-import numpy as np
 from popsynth.aux_samplers.viewing_angle_sampler import ViewingAngleSampler
 
-from hypothesis import given, settings
+from hypothesis import given
 import hypothesis.strategies as st
 
 
-@given(st.floats(min_value=0.01, max_value=90.0),
-       st.integers(min_value=1, max_value=1000))
+@given(
+    st.floats(min_value=0.01, max_value=90.0), st.integers(min_value=1, max_value=1000)
+)
 def test_va_sampler(angle, size):
 
     va_sample = ViewingAngleSampler()
