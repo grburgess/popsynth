42f5a78d04a84cb31e49dec670f068e9be1829b9
grburgess <jmichaelburgess@gmail.com>
2022-02-22 08:23:22 +0100

    blacken

--- popsynth/test/test_sim_vari.py
+++ popsynth/test/test_sim_vari.py
@@ -1,24 +1,21 @@
 import numpy as np
 import numpy.testing as npt
-
 import pytest
 
 from popsynth.simulated_variable import SimulatedVariable
 
-_obs = np.array([1,2,3])
-_latent = np.array([5,6,7])
-_selection = np.array([1,0,1], dtype=bool)
+_obs = np.array([1, 2, 3])
+_latent = np.array([5, 6, 7])
+_selection = np.array([1, 0, 1], dtype=bool)
 
 
-_latent_bad = np.array([5,6])
-_selection_bad = np.array([1,0], dtype=bool)
-
+_latent_bad = np.array([5, 6])
+_selection_bad = np.array([1, 0], dtype=bool)
 
 
 def test_simulated_variable_constructor():
 
-    sv = SimulatedVariable(_obs,_latent, _selection)
-
+    sv = SimulatedVariable(_obs, _latent, _selection)
 
     npt.assert_array_equal(sv.view(np.ndarray), _obs)
 
@@ -30,17 +27,13 @@ def test_simulated_variable_constructor():
 
         SimulatedVariable(_obs, _latent_bad, _selection)
 
-
     with pytest.raises(AssertionError):
 
         SimulatedVariable(_obs, _latent, _selection_bad)
 
-
-
     with pytest.raises(AssertionError):
 
         SimulatedVariable(_obs, _latent_bad, _selection_bad)
-
 
     sel = sv.selected
 
@@ -49,7 +42,6 @@ def test_simulated_variable_constructor():
     npt.assert_array_equal(sel.latent, _latent[_selection])
 
     npt.assert_array_equal(sel._selection, _selection[_selection])
-
 
     nsel = sv.non_selected
 
@@ -60,7 +52,6 @@ def test_simulated_variable_constructor():
     npt.assert_array_equal(nsel._selection, _selection[~_selection])
 
 
-
 def test_simulated_variable_math():
 
     lat2 = np.power(_latent, 2)
@@ -69,14 +60,13 @@ def test_simulated_variable_math():
     obs2 = np.power(_obs, 2)
     obsp1 = _obs + 1
 
-    sv = SimulatedVariable(_obs,_latent, _selection)
+    sv = SimulatedVariable(_obs, _latent, _selection)
 
     sq = np.power(sv, 2)
 
     npt.assert_array_equal(sq.view(np.ndarray), obs2)
 
     npt.assert_array_equal(sq.latent, lat2)
-
 
     sq = sv + 1
 
