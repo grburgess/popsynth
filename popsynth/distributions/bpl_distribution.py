fixed normalization

popsynth/distributions/bpl_distribution.py
@@ -109,20 +109,24 @@ def bpl(x, x0, x1, x2, a1, a2):
     # creatre a holder for the values
     x = np.atleast_1d(x)
 
-    out = np.empty_like(x)
+    out = np.zeros_like(x)
 
     # get the total integral to compute the normalization
     _, _, C = integrate_pl(x0, x1, x2, a1, a2)
     norm = 1.0 / C
 
     # create an index to select each piece of the function
-    idx = x < x1
+    idx = (x > x0) & (x < x1)
 
     # compute the lower power law
     out[idx] = np.power(x[idx], a1)
 
     # compute the upper power law
-    out[~idx] = np.power(x[~idx], a2) * np.power(x1, a1 - a2)
+
+    idx = (x>=x1) & (x<x2)
+
+    out[idx] = np.power(x[idx], a2) * np.power(x1, a1 - a2)
+
 
     return out * norm
 